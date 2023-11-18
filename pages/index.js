5565225fc85e3c4fd66e6769a0607f892b53203c
bhaineeraj <ay685042@gmail.com>
2023-11-18 16:13:04 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {client} from '../lib/client';
+import { Product,HeroBanner,FooterBanner } from '../components'
+const Home = ({products,bannerData}) => {
+  return (
+  <div>
+  <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+   {console.log(bannerData)}
+   {console.log(products)}
+  <div className='products-heading'>
+    <h2>
+      Best selling headphones
+    </h2>
+    <p>
+      speakers of variations
+    </p>
+  </div>
+  <div className='products-container'>
+      {products?.map((product)=><Product key={product._id} product={product}/>)}
+  </div>
+  <FooterBanner footerBanner={bannerData&&bannerData[0]}/>
+  </div>
+  )
+}
+
+export const getServerSideProps=async()=>{
+  const query = '*[_type=="product"]';
+  const products = await client.fetch(query);
+  
+  const bannerQuery = '*[_type=="banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props:{products,bannerData}
+  }
+}
+export default Home;
